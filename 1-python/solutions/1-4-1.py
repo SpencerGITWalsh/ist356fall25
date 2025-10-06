e8716a5757dde0c21cd7f58b79f9de6a489a8162
SpencerGITWalsh <rswalsh@syr.edu>
2025-10-06 15:54:45 -0400

    Commit

--- 1-python/solutions/1-4-1.py
+++ 1-python/solutions/1-4-1.py
@@ -1,5 +1,12 @@
-import datetime
+from datetime import datetime
+today=datetime.now()
+print(today.day)
 
 
+birthday = input("Enter your birthday : ")
+test = datetime.strptime(birthday, "%Y-%m-%d")
+print(test)
+
+from dateutils import parsedate_mdy, fortmatdate_ymd
 
                          
